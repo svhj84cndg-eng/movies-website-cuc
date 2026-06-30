385d11765cf96e6376c778a9c1c565f6c366822f
svhj84cndg-eng <svhj84cndg@privaterelay.appleid.com>
2026-06-30 22:23:08 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/movies-website-cuc',
-  assetPrefix: '/movies-website-cuc/',
+  output: 'export', // 关键：静态导出纯HTML，适配GitHub Pages
   trailingSlash: true,
-  images: { unoptimized: true }
 }
+
 module.exports = nextConfig
